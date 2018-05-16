6e91d25b671323494c382a5d4511516d3e68a75d
huuthai37 <huuthai37@gmail.com>
2018-05-17 04:01:19 +0700

    Change

--- get_data.py
+++ get_data.py
@@ -91,6 +91,7 @@ def stack_seq_rgb(path_video,render_rgb,pre_random,dataset,train):
             rgb = rgb.astype('float16',copy=False)
             rgb/=255
             rgb_nor = rgb - rgb.mean(axis=2, keepdims=True)
+            print rgb_nor.shape
         else:
             print(mode_crop, flip, mode_corner_crop, size, height, x, y)
             sys.exit()

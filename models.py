3d2673f518f70d60fe66363d0b2054d672c8e9b3
Thái Hữu <huuthai37@gmail.com>
2018-05-18 10:42:20 +0700

    Update models.py

--- models.py
+++ models.py
@@ -93,7 +93,7 @@ def SpatialConsensus2(seq_len=3, classes=101, weights='imagenet', dropout=0.5):
 
     z = Average()([y_1, y_2, y_3])
     z = Dropout(dropout, name='dropout')(z)
-    z = Activation('relu')(z)
+    z = Activation('softmax')(z)
 
     result_model = Model(inputs=[input_1, input_2, input_3], outputs=z)
 

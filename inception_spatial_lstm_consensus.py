acfa8c78cb6df56f34a33a058d278eb6668961dd
Thái Hữu <huuthai37@gmail.com>
2018-05-21 15:59:47 +0700

    Update inception_spatial_lstm_consensus.py

--- inception_spatial_lstm_consensus.py
+++ inception_spatial_lstm_consensus.py
@@ -8,8 +8,8 @@ parser.add_argument('-e', '--epoch', help='Number of epochs', default=5, type=in
 parser.add_argument('-r', '--retrain', help='Number of old epochs when retrain', default=0, type=int)
 parser.add_argument('-cross', '--cross', help='Cross fold', default=1, type=int)
 parser.add_argument('-s', '--summary', help='Show model', default=0, type=int)
-parser.add_argument('-lr', '--lr', help='Learning rate', default=1e-3, type=float)
-parser.add_argument('-decay', '--decay', help='Decay', default=1e-6, type=float)
+parser.add_argument('-lr', '--lr', help='Learning rate', default=1e-4, type=float)
+parser.add_argument('-decay', '--decay', help='Decay', default=0.0, type=float)
 parser.add_argument('-fine', '--fine', help='Fine-tuning', default=1, type=int)
 args = parser.parse_args()
 print args
@@ -53,7 +53,7 @@ else:
     fine = False
 
 result_model = models.InceptionSpatialLSTMConsensus(
-                    n_neurons=n_neurons, seq_len=seq_len, classes=classes, weights=weights, dropout=dropout, fine=fine)
+                    n_neurons=n_neurons, seq_len=seq_len, classes=classes, weights=weights, dropout=dropout, fine=False)
 
 
 if (args.summary == 1):
@@ -62,16 +62,28 @@ if (args.summary == 1):
 
 lr = args.lr 
 decay = args.decay
-
-result_model.compile(loss='categorical_crossentropy',
-                     optimizer=optimizers.SGD(lr=lr, decay=decay, momentum=0.9, nesterov=True),
-                     metrics=['accuracy'])
     
 
 if train:
+    result_model.summary()
+    result_model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=["accuracy"])
+    models.train_process(result_model, pre_file, data_type=[0], epochs=3, dataset=dataset,
+        retrain=retrain,  classes=classes, cross_index=cross_index, 
+        seq_len=seq_len, old_epochs=0, batch_size=batch_size, fine=False)
+    
+    # Retrain without preeze some layers
+    for layer in result_model.layers[:172]:
+        layer.trainable = False
+    for layer in result_model.layers[172:]:
+        layer.trainable = True
+    result_model.get_layer('batch_normalization_1').trainable = True
+    result_model.summary()
+    result_model.compile(loss='categorical_crossentropy',
+                     optimizer=optimizers.SGD(lr=lr, decay=decay, momentum=0.9, nesterov=True),
+                     metrics=['accuracy'])
     models.train_process(result_model, pre_file, data_type=[0], epochs=epochs, dataset=dataset,
         retrain=retrain,  classes=classes, cross_index=cross_index, 
-        seq_len=seq_len, old_epochs=old_epochs, batch_size=batch_size, fine=False)
+        seq_len=seq_len, old_epochs=3, batch_size=batch_size, fine=False)
 
 else:
     models.test_process(result_model, pre_file, data_type=[0], epochs=epochs, dataset=dataset,

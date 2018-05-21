6e9c1a3a4e9e395dc77e903f52f727afb73f1105
Thái Hữu <huuthai37@gmail.com>
2018-05-21 20:02:14 +0700

    Update inception_spatial_lstm_consensus.py

--- inception_spatial_lstm_consensus.py
+++ inception_spatial_lstm_consensus.py
@@ -94,7 +94,7 @@ if train:
         layer.trainable = True
     inception.get_layer('batch_normalization_1').trainable = True
     if retrain:
-        model.load_weights('weights/{}_{}e_cr{}.h5'.format(pre_file,old_epochs,cross_index))
+        result_model.load_weights('weights/{}_{}e_cr{}.h5'.format(pre_file,old_epochs,cross_index))
         if not fine:
             for layer in inception.layers:
                 layer.trainable = True

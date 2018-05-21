51674dd312be60500370e077d4b97c0674a9e03f
Thái Hữu <huuthai37@gmail.com>
2018-05-21 20:34:04 +0700

    Update inception_spatial_lstm_consensus.py

--- inception_spatial_lstm_consensus.py
+++ inception_spatial_lstm_consensus.py
@@ -8,7 +8,7 @@ parser.add_argument('-e', '--epoch', help='Number of epochs', default=5, type=in
 parser.add_argument('-r', '--retrain', help='Number of old epochs when retrain', default=0, type=int)
 parser.add_argument('-cross', '--cross', help='Cross fold', default=1, type=int)
 parser.add_argument('-s', '--summary', help='Show model', default=0, type=int)
-parser.add_argument('-lr', '--lr', help='Learning rate', default=1e-4, type=float)
+parser.add_argument('-lr', '--lr', help='Learning rate', default=1e-3, type=float)
 parser.add_argument('-decay', '--decay', help='Decay', default=0.0, type=float)
 parser.add_argument('-fine', '--fine', help='Fine-tuning', default=1, type=int)
 args = parser.parse_args()
@@ -63,8 +63,10 @@ inception = InceptionV3(
 )
 result_model = Sequential()
 result_model.add(TimeDistributed(inception, input_shape=(seq_len, 224,224,3)))
+result_model.add(BatchNormalization())
 result_model.add(LSTM(n_neurons, return_sequences=True))
 result_model.add(Flatten())
+result_model.add(BatchNormalization())
 result_model.add(Dropout(dropout))
 result_model.add(Dense(classes, activation='softmax'))
 
@@ -74,37 +76,13 @@ if (args.summary == 1):
 
 lr = args.lr 
 decay = args.decay
-
-if train:
-    if not retrain:
-        for layer in inception.layers:
-            layer.trainable = False
-        result_model.summary()
-        result_model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=["accuracy"])
-        models.train_process(result_model, pre_file, data_type=[0], epochs=3, dataset=dataset,
-            retrain=retrain,  classes=classes, cross_index=cross_index, 
-            seq_len=seq_len, old_epochs=0, batch_size=batch_size, fine=False)
-        old_epochs = 3
-    
-    # Retrain without preeze some layers
-    
-    for layer in inception.layers[:172]:
-        layer.trainable = False
-    for layer in inception.layers[172:]:
-        layer.trainable = True
-    inception.get_layer('batch_normalization_1').trainable = True
-    if retrain:
-        result_model.load_weights('weights/{}_{}e_cr{}.h5'.format(pre_file,old_epochs,cross_index))
-        if not fine:
-            for layer in inception.layers:
-                layer.trainable = True
-    result_model.summary()
-    result_model.compile(loss='categorical_crossentropy',
+result_model.compile(loss='categorical_crossentropy',
                      optimizer=optimizers.SGD(lr=lr, decay=decay, momentum=0.9, nesterov=True),
                      metrics=['accuracy'])
+if train:
     models.train_process(result_model, pre_file, data_type=[0], epochs=epochs, dataset=dataset,
         retrain=retrain,  classes=classes, cross_index=cross_index, 
-        seq_len=seq_len, old_epochs=old_epochs, batch_size=batch_size, fine=False)
+        seq_len=seq_len, old_epochs=old_epochs, batch_size=batch_size)
 
 else:
     models.test_process(result_model, pre_file, data_type=[0], epochs=epochs, dataset=dataset,
